2195cb21ec3b9ca68ef45ab3a1c7e40aa6a5d358
Meteor <coopticks@outlook.com>
2021-12-14 08:16:21 +0800

    Update JavBusCrawler.py

--- JavBusCrawler.py
+++ JavBusCrawler.py
@@ -125,7 +125,7 @@ if __name__ == "__main__":
                             FileList.append([float(_file.text.replace(" ", "").replace("GB", "")), _file.get_attribute("href")])
                             #print(_file.text.replace(" ", "").replace("GB", ""))
                         elif re.match(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?MB', _file.text):
-                            FileList.append([float(_file.text.replace(" ", "").replace("MB", "")), _file.get_attribute("href")])
+                            FileList.append([float(_file.text.replace(" ", "").replace("MB", ""))/1024, _file.get_attribute("href")])
                     client.addUri(sorted(FileList, key=lambda x: float(x[0]), reverse=True)[0][1])
                 time.sleep(0.6)
             driver.close()

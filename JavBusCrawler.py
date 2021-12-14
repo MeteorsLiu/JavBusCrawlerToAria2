38c218d10f7a5e4835a05219ee568138fda91b16
Meteor <coopticks@outlook.com>
2021-12-14 08:34:02 +0800

    添加匹配整形文件大小

--- JavBusCrawler.py
+++ JavBusCrawler.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 import json
 import os , re
-import codecs
 import requests
 from pip._vendor.distlib.compat import raw_input
 import random
@@ -121,10 +120,10 @@ if __name__ == "__main__":
                     continue
                 else:
                     for _file in ele:
-                        if re.match(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?GB', _file.text) or re.match("[0-9]/d+GB"):
+                        if re.match(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?GB', _file.text) or re.match("[0-9]+GB$"):
                             FileList.append([float(_file.text.replace(" ", "").replace("GB", "")), _file.get_attribute("href")])
                             #print(_file.text.replace(" ", "").replace("GB", ""))
-                        elif re.match(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?MB', _file.text) or re.match("[0-9]/d+MB"):
+                        elif re.match(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?MB', _file.text) or re.match("[0-9]+MB$"):
                             FileList.append([float(_file.text.replace(" ", "").replace("MB", ""))/1024, _file.get_attribute("href")])
                     client.addUri(sorted(FileList, key=lambda x: float(x[0]), reverse=True)[0][1])
                 time.sleep(0.6)
